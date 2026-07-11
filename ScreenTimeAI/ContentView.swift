f68b0dbe4d8fbd7c7de78a593d097db37ebbc2ef
PRASAD K V - T0240U6 <Kumaraprasad.mac@gmail.com>
2026-07-11 16:03:56 +0530

    Initial commit: ScreenTimeAI Xcode project scaffold
    Default SwiftUI iOS app template, ready for Screen Time data pipeline work.

---
+++ ScreenTimeAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTimeAI
+//
+//  Created by T0240U6 on 11/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTimeAI/ScreenTimeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeAIApp.swift
+//  ScreenTimeAI
+//
+//  Created by T0240U6 on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
